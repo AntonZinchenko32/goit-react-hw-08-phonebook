fabd765ebdb6fd74daa630cb86b4d5894be86040
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-19 19:35:41 +0300

    user login realizee

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { signUp } from 'services/users-api';
+import { signUp, signIn } from 'services/users-api';
 
 export const register = createAsyncThunk(
   'auth/register',
   ({ name, email, password }) => signUp(name, email, password)
+);
+
+export const logIn = createAsyncThunk('auth/login', ({ email, password }) =>
+  signIn(email, password)
 );
--- src/services/users-api.js
+++ src/services/users-api.js
@@ -1,9 +1,26 @@
-import axios from "axios";
+import axios from 'axios';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/users/';
 
+// Utility to add JWT
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+// Utility to remove JWT
+// const clearAuthHeader = () => {
+//   axios.defaults.headers.common.Authorization = '';
+// };
+
 export const signUp = async (name, email, password) => {
-    console.log({name, email, password});
-  const {data} = await axios.post('/signup', {name, email, password});
+  console.log({ name, email, password });
+  const { data } = await axios.post('/signup', { name, email, password });
+  setAuthHeader(data.token);
+  return data;
+};
+
+export const signIn = async (email, password) => {
+  const { data } = await axios.post('/login', { email, password });
+  setAuthHeader(data.token);
   return data;
 };
