57912fa76d9c1aef17ce809610af712388b07ba3
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-21 02:09:51 +0300

    Update slice.js

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -14,7 +14,7 @@ const initialState = {
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
-  isLoading: false
+  isLoading: false,
 };
 
 const authSlice = createSlice({
@@ -30,20 +30,18 @@ const authSlice = createSlice({
       .addCase(refreshUser.rejected, state => {
         state.isRefreshing = false;
       })
-      .addCase(logIn.rejected, state =>
-      {
-        state.isLoading = false
-        return handleReject('User not found or wrong password')
-      }
-      )
-      .addCase(register.rejected, state =>
-      {
-        state.isLoading = false
-        return handleReject('This email is already registered')
-      }
-      )
+      .addCase(logIn.rejected, state => {
+        state.isLoading = false;
+        return handleReject('User not found or wrong password');
+      })
+      .addCase(register.rejected, state => {
+        state.isLoading = false;
+        return handleReject('This email is already registered');
+      })
       .addMatcher(isFulfilled(register, logIn), handleLogIn)
-      .addMatcher(isPending(register, logIn), state => {state.isLoading = true})
+      .addMatcher(isPending(register, logIn), state => {
+        state.isLoading = true;
+      });
   },
 });
 
