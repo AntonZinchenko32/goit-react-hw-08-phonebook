c65000a1eb86bf5a1899fb32b343fa2b30505a8d
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-23 00:02:40 +0300

    Update AppBar.styled.js

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -7,7 +7,7 @@ export const Header = styled.header`
   gap: 12px;
   padding: 8px 0;
   margin-bottom: 16px;
-  border-bottom: 2px solid black;
+  border-bottom: 1px solid black;
 
   > nav {
     display: flex;
