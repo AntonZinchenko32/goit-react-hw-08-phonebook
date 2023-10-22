9ae355ea9e89515ac21c4dbc717e81d1edce213e
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-22 18:12:11 +0300

    Update operations.js

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,15 +1,18 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { fetchAll, addItem, deleteItem } from 'api/contacts';
 
+// Fetch All
 export const fetchContacts = createAsyncThunk('contacts/fetchAll', () =>
   fetchAll()
 );
 
+// Add One
 export const addContact = createAsyncThunk(
   'contacts/addContact',
   ({ name, number }) => addItem(name, number)
 );
 
+// Delete One
 export const deleteContact = createAsyncThunk('contacts/deleteContact', id =>
   deleteItem(id)
 );
