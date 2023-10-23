960a3e58c35fd2ad9c3c3278b97c7d097803f49d
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-23 20:17:19 +0300

    Styling loginForm

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,6 +1,15 @@
 import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/auth/operations';
-import { Form, Label } from './LoginForm.styled';
+import { Form } from './LoginForm.styled';
+
+// MUI
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+import EmailIcon from '@mui/icons-material/Email';
+import Button from '@mui/material/Button';
+// MUI
 
 export const LoginForm = () => {
   const dispatch = useDispatch();
@@ -18,15 +27,52 @@ export const LoginForm = () => {
 
   return (
     <Form onSubmit={handleSubmit} autoComplete="off">
-      <Label>
-        Email
-        <input type="email" name="email" />
-      </Label>
-      <Label>
-        Password
-        <input type="password" name="password" />
-      </Label>
-      <button type="submit">Log In</button>
+      <Box sx={{ '& > :not(style)': { m: 2 } }}>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'flex-end',
+          }}
+        >
+          <EmailIcon
+            sx={{
+              color: 'action.active',
+              mr: 1,
+              my: 0.5,
+            }}
+          />
+          <TextField
+            sx={{ width: '100%' }}
+            name="email"
+            type="email"
+            label="E-mail"
+            variant="standard"
+          />
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+          <VpnKeyIcon
+            sx={{
+              color: 'action.active',
+              mr: 1,
+              my: 0.5,
+            }}
+          />
+          <TextField
+            sx={{ width: '100%' }}
+            name="password"
+            label="password"
+            variant="standard"
+            type="password"
+          />
+        </Box>
+      </Box>
+      <Button
+        sx={{ margin: '0 auto', width: '150px' }}
+        type="submit"
+        variant="contained"
+      >
+        LogIn
+      </Button>
     </Form>
   );
 };
